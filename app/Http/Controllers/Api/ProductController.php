de584b2e50cd79d97e47ad058c0a05da8afc3a98
Mjdarkhorse <ms54899@gmail.com>
2025-11-15 15:30:49 +0530

    crude opration api

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Product::all(), 200);
+    }
+
+    // Create
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'sku' => 'required|unique:products',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'message' => 'Product created successfully',
+            'data' => $product
+        ], 201);
+    }
+
+    // Get single
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json($product);
+    }
+
+    // Update
+   public function update(Request $request, $id)
+{
+    $product = Product::find($id);
+
+    if (!$product) {
+        return response()->json(['message' => 'Product not found'], 404);
+    }
+
+    $data = $request->validate([
+        'name' => 'sometimes|required',
+        'sku' => 'sometimes|required|unique:products,sku,' . $id,
+        'price' => 'sometimes|numeric',
+        'stock' => 'sometimes|numeric'
+    ]);
+
+    $product->update($data);
+
+    return response()->json([
+        'message' => 'Product updated successfully',
+        'data' => $product
+    ], 200);
+}
+
+
+
+    // Delete
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+use App\Http\Controllers\Api\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);     // All
+Route::post('/products', [ProductController::class, 'store']);    // Create
+Route::get('/products/{id}', [ProductController::class, 'show']); // Single
+Route::put('/products/{id}', [ProductController::class, 'update']); // Update
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); // Delete
+
